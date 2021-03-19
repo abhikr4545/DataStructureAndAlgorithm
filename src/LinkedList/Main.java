1f61b9e9db9eb9ee2ffa1743441c0a8aa4bd77f7
abhikr4545 <54454911+abhikr4545@users.noreply.github.com>
2021-03-19 14:29:33 +0530

    Update Main.java

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         List linkedList = new List();
 
-
+        /*Comment*/
         linkedList.addToList(5);
         linkedList.addToFront(1);
         linkedList.addToBack(6);
